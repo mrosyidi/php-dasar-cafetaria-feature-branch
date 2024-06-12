4b428639dda3e28688cd8141382de06c46cba6f6
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-12 11:11:15 +0700

    update App.php require RemoveFood.php

--- App.php
+++ App.php
@@ -3,5 +3,6 @@
   require_once __DIR__ . "/Model/Foods.php";
   require_once __DIR__ . "/BusinessLogic/ShowFood.php";
   require_once __DIR__ . "/BusinessLogic/AddFood.php";
+  require_once __DIR__ . "/BusinessLogic/RemoveFood.php";
 
   echo "Aplikasi Todolist" . PHP_EOL;
