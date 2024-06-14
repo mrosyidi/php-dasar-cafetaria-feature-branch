f7eeb107377d69f54fcaf7cd212a1d3a4c58bcc3
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-14 08:34:54 +0700

    update ViewShowFood.php menampilkan daftar makanan dan menu pilihan

--- View/ViewShowFood.php
+++ View/ViewShowFood.php
@@ -1,6 +1,35 @@
 <?php
 
+  require_once __DIR__ . "/../BusinessLogic/ShowFood.php";
+  require_once __DIR__ . "/../View/ViewMainMenu.php";
+  require_once __DIR__ . "/../Helper/Input.php";
+
   function viewShowFood(): void
   {
-    
+    while(true)
+    {
+      showFood();
+
+      echo "Menu Makanan" . PHP_EOL;
+      echo "1. Tambah Makanan" . PHP_EOL;
+      echo "2. Hapus Makanan" . PHP_EOL;
+      echo "x. Kembali" . PHP_EOL;
+
+      $pilihan = input("Pilih");
+
+      if($pilihan == "1")
+      {
+
+      }else if($pilihan == "2")
+      {
+
+      }else if($pilihan == "x")
+      {
+        viewMainMenu();
+        exit;
+      }else
+      {
+        echo "Pilihan tidak dimengerti" . PHP_EOL;
+      }
+    }
   }
