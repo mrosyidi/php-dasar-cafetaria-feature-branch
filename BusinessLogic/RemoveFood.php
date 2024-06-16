09b80561433d4411976f9edc77c1b18065cae363
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-16 10:28:33 +0700

    update RemoveFood.php menghapus makanan

--- BusinessLogic/RemoveFood.php
+++ BusinessLogic/RemoveFood.php
@@ -2,5 +2,26 @@
 
   function removeFood(int $number): bool
   {
-    
+    global $foods;
+
+    $elements = is_array($foods) ? sizeof($foods) : 0;
+
+    if($number <= 0)
+    {
+      return false;
+    }
+
+    if($number > $elements)
+    {
+      return false;
+    }
+
+    for($index = $number; $index < $elements; $index++)
+    {
+      $foods[$index]["name"] = $foods[$index+1]["name"];
+      $foods[$index]["price"] = $foods[$index+1]["price"];
+    }
+
+    unset($foods[$elements]);
+    return true;
   }
