24297334404f82c293802f2a7e3087d88df8af03
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-17 08:22:16 +0700

    update App.php require Drinks.php

--- App.php
+++ App.php
@@ -1,6 +1,7 @@
 <?php
 
   require_once __DIR__ . "/Model/Foods.php";
+  require_once __DIR__ . "/Model/Drinks.php";
   require_once __DIR__ . "/BusinessLogic/ShowFood.php";
   require_once __DIR__ . "/BusinessLogic/AddFood.php";
   require_once __DIR__ . "/BusinessLogic/RemoveFood.php";
