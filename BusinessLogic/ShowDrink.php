f0808799cb21c091f4b4bdee7dcd43b82a6426e9
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-17 16:32:34 +0700

    update ShowDrink.php menampilkan daftar minuman

--- BusinessLogic/ShowDrink.php
+++ BusinessLogic/ShowDrink.php
@@ -2,5 +2,18 @@
 
   function showDrink(): void
   {
+    global $drinks;
 
+    echo "DAFTAR MINUMAN" . PHP_EOL;
+
+    if(!empty($drinks))
+    {
+      foreach($drinks as $number => $drink)
+      {
+        echo "$number. " . $drink["name"] . "  Rp.  " . $drink["price"] . PHP_EOL;
+      }
+    }else
+    {
+      echo "Tidak ada daftar minuman" . PHP_EOL;
+    }
   }
