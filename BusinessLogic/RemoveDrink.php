5c7806e79e759726f108bcf0f242145fb6d7bc35
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-19 10:13:50 +0700

    update RemoveDrink.php menghapus minuman

--- BusinessLogic/RemoveDrink.php
+++ BusinessLogic/RemoveDrink.php
@@ -2,5 +2,26 @@
 
   function removeDrink(int $number): bool
   {
+    global $drinks;
 
+    $elements = is_array($drinks) ? sizeof($drinks) : 0;
+
+    if($number <= 0)
+    {
+      return false;
+    }
+
+    if($number > $elements)
+    {
+      return false;
+    }
+
+    for($index = $number; $index < $elements; $index++)
+    {
+      $drinks[$index]["name"] = $drinks[$index+1]["name"];
+      $drinks[$index]["price"] = $drinks[$index+1]["price"];
+    }
+
+    unset($drinks[$elements]);
+    return true;
   }
