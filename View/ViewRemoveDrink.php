4440e9fd0957927235d22a99dd14baa0594e2d82
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-19 12:10:35 +0700

    update ViewRemoveDrink.php proses menghapus minuman

--- View/ViewRemoveDrink.php
+++ View/ViewRemoveDrink.php
@@ -1,6 +1,30 @@
 <?php
 
+  require_once __DIR__ . "/../BusinessLogic/RemoveDrink.php";
+  require_once __DIR__ . "/../Helper/Input.php";
+
   function viewRemoveDrink(): void
   {
+    echo "MENGHAPUS MINUMAN" . PHP_EOL;
 
+    $number = input("Nomor (x untuk batal)");
+
+    if($number == "x")
+    {
+      echo "Batal menghapus minuman" . PHP_EOL;
+    }else if(!is_numeric($number))
+    {
+      echo "Gagal menghapus minuman, nomor harus bilangan" . PHP_EOL;
+    }else
+    {
+      $success = removeDrink($number);
+
+      if($success)
+      {
+        echo "Sukses menghapus minuman nomor $number" . PHP_EOL;
+      }else
+      {
+        echo "Gagal menghapus minuman nomor $number" . PHP_EOL;
+      }
+    }
   }
