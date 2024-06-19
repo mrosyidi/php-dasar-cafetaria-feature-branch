8838a6b252b3e79b3e0e54b55e8e63e2e5de8e9f
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-19 12:13:43 +0700

    add TestViewRemoveDrink.php

---
+++ Test/TestViewRemoveDrink.php
@@ -0,0 +1,27 @@
+<?php
+
+  require_once __DIR__ . "/../View/ViewRemoveDrink.php";
+  require_once __DIR__ . "/../BusinessLogic/ShowDrink.php";
+  require_once __DIR__ . "/../BusinessLogic/AddDrink.php";
+
+  showDrink();
+  viewRemoveDrink();
+
+  showDrink();
+  viewRemoveDrink();
+
+  showDrink();
+  viewRemoveDrink();
+
+  showDrink();
+  viewRemoveDrink();
+
+  addDrink("Es Coklat", 12000);
+  showDrink();
+  viewRemoveDrink();
+
+  showDrink();
+  viewRemoveDrink();
+
+  showDrink();
+  viewRemoveDrink();
