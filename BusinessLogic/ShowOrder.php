e05d2613babb2066dbf37a22eaad89ae43485032
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-06-22 10:43:10 +0700

    update ShowOrder.php menampilkan daftar pemesanan

--- BusinessLogic/ShowOrder.php
+++ BusinessLogic/ShowOrder.php
@@ -2,5 +2,18 @@
 
   function showOrder(): void
   {
-    
+    global $orders;
+
+    echo "DAFTAR PESANAN" . PHP_EOL;
+
+    if(!empty($orders))
+    {
+      foreach($orders as $number => $order)
+      {
+        echo "$number. " . $order["code"] . " " . $order["name"] . " Rp." . $order["price"] . " (x" . $order["qty"] . ") Rp." . $order["sub_total"] . PHP_EOL;
+      }
+    }else
+    {
+      echo "Tidak ada daftar pesanan" . PHP_EOL;
+    }
   }
